7036094545404c610a0bafeeb6c6d53e665e3870
stanleysu <stanley_su@live.com>
2016-10-04 17:12:23 -0400

    tdidf vectorizer with parameters

--- stanley/main.py
+++ stanley/main.py
@@ -1,10 +1,15 @@
 import csv
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
-
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score
+import string
+import nltk
+#exclude = set([",",".","\"","|","/","'","?","!","then","and","an","a","so","the","we"])
 
 train_data = []
 target_data = []
@@ -14,7 +19,8 @@ with open('../train_in.csv', 'rt') as csvfile:
 	next(reader)
 	for row in reader:
 		train_data.append(row[1])
-
+		#s = ''.join(ch for ch in row[1] if ch not in exclude)
+		#train_data.append(s.lower())
 
 with open('../train_out.csv', 'rt') as csvfile:
 	reader = csv.reader(csvfile, delimiter=',')
@@ -28,19 +34,48 @@ with open('../test_in.csv', 'rt') as csvfile:
 	next(reader)
 	for row in reader:
 		test_data.append(row[1])
+		#s = ''.join(ch for ch in row[1] if ch not in exclude)
+		#test_data.append(s.lower())
 
+# Straight up naive bayes
+# vectorizer = CountVectorizer()
+# X_train_counts = vectorizer.fit_transform(train_data)
+# tfidf_transformer = TfidfTransformer()
+# X_train_counts = tfidf_transformer.fit_transform(X_train_counts)
 
-vectorizer = CountVectorizer()
-X_train_counts = vectorizer.fit_transform(train_data)
+# clf = MultinomialNB().fit(X_train_counts, target_data)
 
-clf = MultinomialNB().fit(X_train_counts, target_data)
+# X_test_counts = vectorizer.transform(test_data)
+# X_test_counts = tfidf_transformer.transform(X_test_counts)
+# predicted = clf.predict(X_test_counts)
 
-X_test_counts = vectorizer.transform(test_data)
+#added tfidf
+# vectorizer = CountVectorizer()
+# X_train_counts = vectorizer.fit_transform(train_data)
+# tfidf_transformer = TfidfTransformer()
+#X_train_counts = tfidf_transformer.fit_transform(X_train_counts)
+
+tf = TfidfVectorizer(analyzer='word', ngram_range=(1,1), min_df = 0, stop_words = 'english', max_features= 40000)
+
+X_train_counts = tf.fit_transform(train_data)
+
+print(tf.get_feature_names())
+#print(tf.get_params())
+#print(tf.get_stop_words())
+
+# print(len(tf.get_feature_names()))
+# print(len(tf.get_params()))
+clf = MultinomialNB(alpha=0.1).fit(X_train_counts, target_data)
+
+#X_test_counts = vectorizer.transform(test_data)
+#X_test_counts = tfidf_transformer.transform(X_test_counts)
+X_test_counts = tf.transform(test_data)
 predicted = clf.predict(X_test_counts)
-# print(X_test_counts.shape)
-# print(predicted.shape)
 
-f = open('predictions.csv','w')
+scores = cross_val_score(clf, X_train_counts, target_data, cv=5)
+print(scores)
+
+f = open('predictionsMaxFeatures.csv','w')
 f.write("id,category\n")
 for i, row in enumerate(predicted):
     f.write(str(i)+","+predicted[i]+"\n")
---
+++ stanley/svm.py
@@ -0,0 +1,71 @@
+import csv
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+import string
+train_data = []
+target_data = []
+test_data = []
+
+#exclude = set(string.punctuation)
+exclude = set([",",".","\"","|","/","'","?","!","then","and","an","a","so","the","we"])
+
+with open('../train_in.csv', 'rt') as csvfile:
+	reader = csv.reader(csvfile, delimiter=',')
+	next(reader)
+	for row in reader:
+		s = ''.join(ch for ch in row[1] if ch not in exclude)
+		train_data.append(s.lower())
+
+
+with open('../train_out.csv', 'rt') as csvfile:
+	reader = csv.reader(csvfile, delimiter=',')
+	next(reader)
+	for row in reader:
+		target_data.append(row[1])
+
+
+with open('../test_in.csv', 'rt') as csvfile:
+	reader = csv.reader(csvfile, delimiter=',')
+	next(reader)
+	for row in reader:
+		s = ''.join(ch for ch in row[1] if ch not in exclude)
+		test_data.append(s.lower())
+
+# parameters = {'clf__alpha': (1e-2, 1e-3, 1e-3),'clf__n_iter': (2, 4, 5,6,8)}
+
+#text_clf = Pipeline([('vect', CountVectorizer(ngram_range=(1, 2))),  Wasn't effective
+# text_clf = Pipeline([('vect', CountVectorizer()),
+#                       ('tfidf', TfidfTransformer(analyzer='word', ngram_range=(1,1), min_df = 0, stop_words = 'english', max_features= 40000)),
+#                       ('clf', SGDClassifier(loss='hinge', penalty='l2',
+#                                             alpha=1e-3, n_iter=2, random_state=42)),
+# ])
+
+text_clf = Pipeline([('tfidf', TfidfVectorizer(analyzer='word', ngram_range=(1,1), min_df = 0, stop_words = 'english', max_features= 40000)),
+                      ('clf', SGDClassifier(loss='hinge', penalty='l2',
+                                            alpha=1e-3, n_iter=2, random_state=42)),
+])
+
+_ = text_clf.fit(train_data, target_data)
+
+# gs_clf = GridSearchCV(text_clf, parameters, n_jobs=-1)
+# gs_clf = gs_clf.fit(train_data, target_data)
+# for param_name in sorted(parameters.keys()):
+# 	print("%s: %r" % (param_name, gs_clf.best_params_[param_name]))
+
+predicted = text_clf.predict(test_data)
+
+scores = cross_val_score(text_clf, train_data, target_data, cv=5)
+print(scores)
+
+# f = open('predictionsSvm.csv','w')
+# f.write("id,category\n")
+# for i, row in enumerate(predicted):
+#     f.write(str(i)+","+predicted[i]+"\n")
+# f.close()
